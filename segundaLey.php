1f13dba98b4c39e2b4932d071618a0ec9634eb90
Patricio23Cadena <Kastor2345>
2022-03-01 18:02:41 -0500

    ultimo cambio de gif

---
+++ segundaLey.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/segundaLey.css">
+    <title>Segunda Ley de Mendel</title>
+</head>
+<body>
+<header>
+        <div class="icono_home">
+            <a href="index.php"><img src="imagenes/home.png"></a>
+        </div>
+    </header>
+
+    <div class="libreta">
+        <img src="imagenes/libreta.png" style="width: 100%; height: 100%;">
+
+    </div>
+</body>
+</html>
