88c015f12a1467a31f6d3bcf96e9f818c34d70dc
nicasop <nicolas_villita21@hotmail.es>
2022-03-01 14:20:35 -0500

    cambio de extension a php1

---
+++ historia.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estiloh.css">
+    <title>Historia de Mendel </title>
+</head>
+<body>
+        <header>
+            <div class="icono_home">
+                <a href="index.html"><img src="imagenes/home.png"></a>
+            </div>
+            
+            <div id="demotext">HISTORIA DE MENDEL</div></header>
+        <p class="globo i abajo-derecha">
+            Gregor Mendel (1822-1884), un monje Austriaco, es considerado el padre de la Genética.
+             Mendel pasó gran parte de su vida trabajando con chícharos, tratando de revelar el origen de las especies,
+              sin imaginar, que terminaría explicando los principios de heredabilidad de caracteres (Curtis, 2007)
+              .Mendel eligió características que pudieran identificarse claramente para establecer los principios 
+              de heredabilidad, durante varias generaciones. Él evidenció que las intervenciones del padre y de 
+              la madre se manifestaban con desigualdad en los hijos, concluyendo que cada planta recibe características 
+              de cada progenitor.
+
+        </p> 
+       <div class="conte_img">
+        <img src="imagenes/mendel2.jpg" style="width: 100%; height: 100%;">
+
+
+       </div>
+       <section>
+        <iframe  src="https://www.youtube.com/embed/FR8cRbUxZFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+       </section>
+       <div class="slide">
+            <img src="imagenes/slide2_1.gif">
+       </div>
+     
+</body>
+</html>
---
+++ primeraLey.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/primeraLey.css">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Tangerine">
+    <title>Primera Ley de Mendel</title>
+</head>
+<body>
+    <header>
+        <div class="icono_home">
+            <a href="index.html"><img src="imagenes/home.png"></a>
+        </div>
+    </header>
+    <div class="libreta">
+        <img src="imagenes/libreta.png" style="width: 100%; height: 100%;">
+    </div>
+
+    <div class="bandera1">
+        <img src="imagenes/separador.png" style="width: 100%; height: 100%;">
+
+    </div>
+
+    <div class="tabla">
+        <img src="imagenes/tabla LeyesMen_padreGen.png" style="width: 100%; height: 100%;">
+
+    </div>
+    <div class="titulo1">
+        <h1>Primera Ley</h1>
+
+    </div>
+
+
+    <div class="texto">
+        <h1>Primera ley: principio de la uniformidad.</h1>
+        <br>
+        <p> La ley de la uniformidad determina que las razas puras poseen un gen dominante y un gen recesivo y que, al cruzar dos variedades de una especie de raza pura, cada uno de los hijos de la primera generación poseerá representaciones afines en su fenotipo. El genotipo dominante será el que determine el rasgo o rasgos principales de la primera generación del cruce.</p>
+    </div>
+    <div class="imgdoctora">
+        <img src="imagenes/Imagen4.gif" style="width: 100%; height: 100%;">
+        
+    </div>
+    <audio controls >
+        <source  src="audio/primeraLeyAU.mp3" type="audio/ogg">
+        <source  src="audio/primeraLeyAU.mp3" type="audio/mpeg">
+      Your browser does not support the audio element.
+      </audio>
+    <br>
+</body>
+</html>
