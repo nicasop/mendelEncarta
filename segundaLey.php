edc0951872c421a1a3f3d522ad7a69de7317a8e9
Patricio23Cadena <Kastor2345>
2022-03-01 18:12:58 -0500

    cambios del gif

--- segundaLey.php
+++ segundaLey.php
@@ -15,7 +15,7 @@
     </header>
 
     <div class="libreta">
-        <img src="imagenes/libreta.png" style="width: 100%; height: 100%;">
+        <img src="imagenes/libreta2.png" style="width: 100%; height: 100%;">
 
     </div>
 </body>
