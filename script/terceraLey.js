86f1bc3c4d0a7cd04bd3244a6122f61fcff514dd
nicasop <nicolas_villita21@hotmail.es>
2022-03-01 21:58:22 -0500

    tercera ley completo

---
+++ script/terceraLey.js
@@ -0,0 +1,49 @@
+var respuesta = ""
+respuestas =['<section><div class="divD"><iframe src="https://onedrive.live.com/embed?cid=7C880703BBE0C53D&amp;resid=7C880703BBE0C53D%21148&amp;authkey=AFz58AF89IcDGKs&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe></div></section>',
+'<section>'+
+        '<div class="divD">'+
+            '<div class="act1">'+
+            '<p>Se cruza una pareja de perros pastor alemán ambos heterocigotos dominantes de orejas rectas y pelo corto (CcLl) ¿qué probabilidad genotípica y fenotípica tendrá su descendencia?</p>'+
+            '</div>'+
+            '<div class="titulo">Parentales Heterocigotos</div>'+
+            '<img class="img1" src="imagenes/ley3_1.png" alt="imagen de parientes heterocigotos">'+
+            '<div class="act2">'+
+                '<div class="contenido">'+
+                    '<div class="titulo titulo-flex">F2 Cuadros de Punnet</div>'+
+                    '<div class="img-contenido"><img src="imagenes/tabla1ley3.png" alt="cuadro de punnet"></div>'+
+                '</div>'+
+                '<div class="contenido">'+
+                    '<div class="titulo titulo-flex">Fenotipo</div>'+
+                    '<div class="img-contenido"><img src="imagenes/tabla2ley3.png" alt="Fenotipo"></div>'+
+                '</div>'+
+            '</div>'+
+           '<div class="act3">'+
+                '<div class="titulo">Resultados</div>'+   
+                '<div class="titulo">Probabilidad de Genotipo y Fenotípico</div>'+
+                '<div class="imagenes-resultado">'+
+                    '<div class="imagen-resultado"><img src="imagenes/tabla3ley3.png" alt="resultado1"></div>'+
+                    '<div class="imagen-resultado" ><img src="imagenes/tabla4ley3.png" alt="resultado2"></div>'+
+                '</div>'+
+           '</div>'+
+        '</div>'+
+    '</section>'];
+
+$(document).ready(function () {
+    $("#op1").click(function (e) { 
+        e.preventDefault();
+        respuesta = respuestas[0]
+        $('#contenido-boton').empty();
+        $("#contenido-boton").append(respuesta);
+        $('#contenido-boton').hide().fadeIn(2000);
+        $("#link").get(0).click();
+    });
+    $("#op2").click(function (e) { 
+        e.preventDefault();
+        respuesta = respuestas[1]
+        $('#contenido-boton').empty();
+        $("#contenido-boton").append(respuesta).fadeIn(1500);
+        $('#contenido-boton').hide().fadeIn(2000);
+        $("#link").get(0).click();
+    });
+});
+
--- terceraLey.php
+++ terceraLey.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/terceraLey.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@500&display=swap" rel="stylesheet">
     <title>Leyes</title>
 </head>
 <body>
@@ -21,20 +24,15 @@
         <div id="op2" class="menu3d">
             <span>Actividad</span>
         </div>
-        <div id="op2" class="menu3d">
-            <span>Calculadora del cuadro Punnet</span>
+        <div id="op3" class="menu3d">
+            <a href="https://calculadorasonline.com/generador-de-cuadro-de-punnett-online" target="_blanck"><span>Calculadora del cuadro Punnet</span></a>
         </div>
     </div>
     <div class="fantasma">
         <a id="link" href="#contenido-boton"></a>
     </div>
     <div id="contenido-boton"></div>
-    <section>
-        <div class="divD">
-            <iframe src="https://onedrive.live.com/embed?cid=7C880703BBE0C53D&amp;resid=7C880703BBE0C53D%21148&amp;authkey=AFz58AF89IcDGKs&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
-        </div>
-    </section>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<!-- <script type="text/javascript" src="script/experimentos.js"></script> -->
+<script type="text/javascript" src="script/terceraLey.js"></script>
 </html>
