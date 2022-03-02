97ddba065b6f906557fb87d7dc3d2876ab4ffe80
nicasop <nicolas_villita21@hotmail.es>
2022-03-01 20:30:53 -0500

    tercera ley presentación

---
+++ terceraLey.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/terceraLey.css">
+    <title>Leyes</title>
+</head>
+<body>
+    <div class="icono_home">
+        <a href="index.php"><img src="imagenes/home.png"></a>
+    </div>
+    <header>
+        <div id="demotext">La ley de la Herencia Independiente de Caracteres o Ley de la Asociación Independiente</div>
+    </header>
+    <div class="menu">
+        <div id="op1" class="menu3d" >
+            <span>Infografía</span>
+        </div>
+        <div id="op2" class="menu3d">
+            <span>Actividad</span>
+        </div>
+        <div id="op2" class="menu3d">
+            <span>Calculadora del cuadro Punnet</span>
+        </div>
+    </div>
+    <div class="fantasma">
+        <a id="link" href="#contenido-boton"></a>
+    </div>
+    <div id="contenido-boton"></div>
+    <section>
+        <div class="divD">
+            <iframe src="https://onedrive.live.com/embed?cid=7C880703BBE0C53D&amp;resid=7C880703BBE0C53D%21148&amp;authkey=AFz58AF89IcDGKs&amp;em=2&amp;wdAr=1.7777777777777777" frameborder="0">Esto es un documento de <a target="_blank" href="https://office.com">Microsoft Office</a> incrustado con tecnología de <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+        </div>
+    </section>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- <script type="text/javascript" src="script/experimentos.js"></script> -->
+</html>
